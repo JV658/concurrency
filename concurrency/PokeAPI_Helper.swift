390637bfce6a086fcef0463b24a34b3e4c4962e0
JV658 <90655082+JV658@users.noreply.github.com>
2023-05-10 19:36:02 -0400

    made changes so we can query specific pokemon

--- concurrency/PokeAPI_Helper.swift
+++ concurrency/PokeAPI_Helper.swift
@@ -8,12 +8,35 @@
 import Foundation
 
 class PokeAPI_Helper {
-    private static let pokeURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    private static let urlString = "https://pokeapi.co/api/v2/pokemon-species"
     
-    public static func fetchPokemon() async -> Any {
+    private static func createURL(pokemon: String) -> URL?{
+        var tempURLString = urlString
+        if pokemon != "" {
+            tempURLString.append("/")
+            tempURLString.append(pokemon)
+        }
+        guard
+            let url = URL(string: tempURLString)
+        else { return nil }
+        
+        return url
+        
+    }
+    
+    public static func fetch(pokemon: String) async -> Any {
+        let pokeURL = createURL(pokemon: pokemon)!
         let (data, _) = try! await URLSession.shared.data(from: pokeURL)
         
         let jsonObject = try! JSONSerialization.jsonObject(with: data)
         return jsonObject
     }
 }
+
+/*
+ 
+ when the button is pressed
+ display the API response (pokeAPI data) to the console.
+ 
+ extra task - get information for your favorite pokemon and display it when the button is pressed
+ */
--- concurrency/ViewController.swift
+++ concurrency/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pokeName: UITextField!
+    
     @IBAction func printPokeData(_ sender: Any) {
+        Task {
+            print(await PokeAPI_Helper.fetch(pokemon: pokeName.text!))
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
